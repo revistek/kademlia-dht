d1abd4ae75e51f4e9e27b7b3a97b433e4e9da460
Chuong Ngo <cngo.github@gmail.com>
2022-05-06 22:37:48 -0400

    Added function to parse a hex string into an ID.

--- src/node/node_id.rs
+++ src/node/node_id.rs
@@ -1,5 +1,7 @@
 // The Node ID object.
 // Represents a 160-bit number.
+use regex::Regex;
+
 #[derive(Debug, PartialEq)]
 pub struct NodeId([u8; 20]);
 
@@ -11,6 +13,14 @@ pub enum IdError {
 
 const MIN: NodeId = NodeId([0; 20]);
 const MAX: NodeId = NodeId([255; 20]);
+
+fn remove_hex_header(hex_string: &str) -> &str {
+    // Remove the hex string header (i.e., 0x).
+    let mut characters = hex_string.chars();
+    characters.next();
+    characters.next();
+    characters.as_str()
+}
 
 impl NodeId {
     // Builds an Id based on a base-10 number representation.
@@ -63,6 +73,74 @@ impl NodeId {
 
         Ok(new_id)
     }
+
+    // Builds an Id based on a hexadecimal number representation (i.e., 0x####).
+    pub fn from_hex(hex_number: &str) -> Result<NodeId, IdError> {
+        let re = Regex::new("^0x[0-9A-Fa-f]+$").unwrap();
+        let number_string;
+
+        if re.is_match(hex_number) == true {
+            number_string = remove_hex_header(hex_number);
+        } else {
+            // Invalid hex string.
+            return Err(IdError::InvalidValue(String::from(
+                "The input is not a valid hex string.",
+            )));
+        }
+
+        let number_bytes = number_string.to_string().into_bytes();
+
+        let mut new_id = NodeId([0; 20]); // The node ID object to be returned.
+        let mut temp: u128 = 0;
+        let mut id_arr_idx: usize = 19; // The index of the array to update.
+        let ascii_num_base_codepoint = 48; // The base ASCII codepoint for numbers (i.e., 0x30 since
+                                           // number = 0x30 + number).
+        let ascii_cap_letter_base_codepoint = 65; // The base ASCII codepoint for capital letters
+                                                  // (i.e., 0x41 since letter = 0x41 + letter offset).
+        let ascii_letter_base_codepoint = 97; // The base ASCII codepoint for lowercase letters
+                                              // (i.e., 0x61 since letter = 0x61 + letter offset).
+
+        for (idx, x) in number_bytes.iter().rev().enumerate() {
+            // Iterate through the byte array to build the Id.
+            let current_val;
+
+            if x >= &ascii_letter_base_codepoint {
+                current_val =
+                    ((x - ascii_letter_base_codepoint + 10) as u32) * ((16 as u32).pow(idx as u32));
+            } else if x >= &ascii_cap_letter_base_codepoint {
+                current_val = ((x - ascii_cap_letter_base_codepoint + 10) as u32)
+                    * ((16 as u32).pow(idx as u32));
+            } else {
+                current_val =
+                    ((x - ascii_num_base_codepoint) as u32) * ((16 as u32).pow(idx as u32));
+            }
+            temp += current_val as u128;
+
+            while temp > 255 {
+                if temp < 256 {
+                    new_id.0[id_arr_idx] = temp as u8;
+                    temp = 0;
+                } else {
+                    new_id.0[id_arr_idx] = 255;
+                    temp -= 255;
+                }
+
+                if temp > 0 && id_arr_idx == 0 {
+                    return Err(IdError::ValueOutOfRange(String::from(
+                        "The value is to large!",
+                    )));
+                }
+
+                id_arr_idx -= 1;
+            }
+        }
+
+        if temp > 0 {
+            new_id.0[id_arr_idx] = temp as u8;
+        }
+
+        Ok(new_id)
+    }
 }
 
 #[cfg(test)]
@@ -76,7 +154,7 @@ mod tests {
             Err(IdError::InvalidValue(String::from(
                 "The input string is invalid."
             )))
-        )
+        );
     }
 
     #[test]
@@ -86,7 +164,7 @@ mod tests {
             Err(IdError::ValueOutOfRange(String::from(
                 "The value is to large!",
             )))
-        )
+        );
     }
 
     #[test]
@@ -96,7 +174,7 @@ mod tests {
             Ok(NodeId([
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255
             ]))
-        )
+        );
     }
 
     #[test]
@@ -106,6 +184,60 @@ mod tests {
             Ok(NodeId([
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 255, 255
             ]))
-        )
+        );
+    }
+
+    #[test]
+    fn from_hex_invalid_input() {
+        assert_eq!(
+            NodeId::from_hex("NotAHexString"),
+            Err(IdError::InvalidValue(String::from(
+                "The input is not a valid hex string."
+            )))
+        );
+    }
+
+    #[test]
+    fn from_hex_value_too_large() {
+        assert_eq!(
+            NodeId::from_hex("0x75BCD15"),
+            Err(IdError::ValueOutOfRange(String::from(
+                "The value is to large!",
+            )))
+        );
+
+        assert_eq!(
+            NodeId::from_hex("0x75bcd15"),
+            Err(IdError::ValueOutOfRange(String::from(
+                "The value is to large!",
+            )))
+        );
+    }
+
+    #[test]
+    fn from_hex_success() {
+        assert_eq!(
+            NodeId::from_hex("0x1FE"),
+            Ok(NodeId([
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255
+            ]))
+        );
+
+        assert_eq!(
+            NodeId::from_hex("0x1fe"),
+            Ok(NodeId([
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255
+            ]))
+        );
+    }
+
+    #[test]
+    fn from_hex_success_partial_remaining() {
+        assert_eq!(
+            NodeId::from_hex("0x212"),
+            Ok(NodeId([
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 255, 255
+            ]))
+        );
     }
 }
